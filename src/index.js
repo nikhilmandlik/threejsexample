1a152268553b32fd0456950b05fa15cc5afdd1e2
Nikhil Mandlik <nikhiljmandlik@gmail.com>
2021-07-07 22:19:50 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+import { modelLoader } from './model-loader';
+
+import Ahivadh from "./models/gltf/Ahivadh/Ahivadh.gltf";
+import Croc from "./models/gltf/Croc/croc.gltf";
+import Narasimha from "./models/gltf/narasimha/narasimha.gltf";
+
+modelLoader(Croc);
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        three: "./src",
+    },
+    mode: "development",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(gltf)$/,
+                use: [
+                    {
+                        loader: "gltf-webpack-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.(bin|png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {}
+                  }
+                ]
+              }
+        ],
+    },
+};

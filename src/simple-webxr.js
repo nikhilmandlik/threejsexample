99fa13ae18142b687491e07d0836527d7e3bed4a
Nikhil Mandlik <nikhiljmandlik@gmail.com>
2021-07-14 22:50:48 -0700

    Added orbital control, fixed host address for npm start.

--- src/simple-webxr.js
+++ src/simple-webxr.js
@@ -2,61 +2,42 @@ import * as THREE from 'three';
 import Ahivadh from "./models/gltf/Ahivadh/Ahivadh.gltf";
 import Croc from "./models/gltf/Croc/croc.gltf";
 import Narasimha from "./models/gltf/narasimha/narasimha.gltf";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
 
 const scene = new THREE.Scene();
-const canvas = document.querySelector("#c");
+const canvas = document.querySelector("#container");
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 renderer.xr.enabled = true;
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-document.body.appendChild( VRButton.createButton( renderer ) );
+document.body.appendChild(VRButton.createButton(renderer));
 
-// {
-//     const planeSize = 50;
-
-//     const loader = new THREE.TextureLoader();
-//     const texture = loader.load(
-//         "https://threejsfundamentals.org/threejs/resources/images/checker.png"
-//     );
-//     texture.wrapS = THREE.RepeatWrapping;
-//     texture.wrapT = THREE.RepeatWrapping;
-//     texture.magFilter = THREE.NearestFilter;
-//     const repeats = planeSize / 2;
-//     texture.repeat.set(repeats, repeats);
-
-//     const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-//     const planeMat = new THREE.MeshPhongMaterial({
-//         map: texture,
-//         side: THREE.DoubleSide,
-//     });
-//     const mesh = new THREE.Mesh(planeGeo, planeMat);
-//     mesh.rotation.x = Math.PI * -0.5;
-//     mesh.position.y = -0.065;
-
-//     scene.add(mesh);
-// }
-
+// Lights
+// HemisphereLight
 const skyColor = 0xb1e1ff; // light blue
 const groundColor = 0xb97a20; // brownish orange
 const intensity = 1;
 const hemisphereLight = new THREE.HemisphereLight(
-                            skyColor,
-                            groundColor,
-                            intensity
-                        );
+    skyColor,
+    groundColor,
+    intensity
+);
 scene.add(hemisphereLight);
 
+// PointLight
 const pointLight = new THREE.PointLight(0xffffff, 1, 100);
 pointLight.position.set(5, 10, -5);
 scene.add(pointLight);
 
+// Load Models using GLTFLoader
 const gltfLoader = new GLTFLoader();
 // let ahivadh;
 // gltfLoader.load(Ahivadh, (gltf) => {
 //     ahivadh = gltf.scene;
+//     ahivadh.name = 'ahivadh';
 //     ahivadh.scale.set(50,50,50);
 //     ahivadh.position.z = -2;
 //     ahivadh.position.y = 1.5;
@@ -64,35 +45,54 @@ const gltfLoader = new GLTFLoader();
 //     scene.add(ahivadh);
 // });
 
-// let croc;
-// gltfLoader.load(Croc, (gltf) => {
-//     croc = gltf.scene;
-//     croc.scale.set(5,5,5);
-//     croc.position.z = -2;
-//     croc.position.y = 0.2;
+let croc;
+gltfLoader.load(Croc, (gltf) => {
+    croc = gltf.scene;
+    croc.name = 'croc';
+    croc.scale.set(5,5,5);
+    croc.position.z = -2;
+    croc.position.y = 0.2;
 
-//     scene.add(croc);
+    scene.add(croc);
+});
+
+// let narasimha;
+// gltfLoader.load(Narasimha, (gltf) => {
+//     const scale = 6;
+//     narasimha = gltf.scene;
+//     narasimha.name = 'narasimha';
+//     narasimha.scale.set(scale,scale,scale);
+//     narasimha.position.z = -1;
+//     narasimha.position.y = -1;
+
+//     scene.add(narasimha);
 // });
 
 
-let narasimha;
-gltfLoader.load(Narasimha, (gltf) => {
-    const scale = 6;
-    narasimha = gltf.scene;
-    narasimha.scale.set(scale,scale,scale);
-    narasimha.position.z = -1;
-    narasimha.position.y = -1;
+// Add Camera
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 2, 6);
 
-    scene.add(narasimha);
-});
+// OrbitControls for non VR mode
+const controls = new OrbitControls(camera, canvas);
+controls.update();
 
+// Animate
+renderer.setAnimationLoop(function () {
+    const ahivadh = scene.getObjectByName('ahivadh');
+    if (ahivadh) {
+        ahivadh.rotation.y += 0.002;
+    }
 
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set( 0, 2, 6 );
+    const croc = scene.getObjectByName('croc');
+    if (croc) {
+        croc.rotation.y += 0.002;
+    }
 
-renderer.setAnimationLoop( function () {
-    // ahivadh.rotation.y += 0.002;
-    // croc.rotation.y += 0.002;
-    narasimha.rotation.y += 0.002;
-	renderer.render( scene, camera );
+    const narasimha = scene.getObjectByName('narasimha');
+    if (narasimha) {
+        narasimha.rotation.y += 0.002;
+    }
+
+    renderer.render(scene, camera);
 });
